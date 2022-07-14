27591d70a6c32d16305afc3aa8a8bf1715d70f14
sunivers <soyoungp02@gmail.com>
2022-07-14 23:43:16 +0900

    create todo

--- soyoung/src/App.tsx
+++ soyoung/src/App.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const onPressEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    /*
+    React projects that don't include the DOM library need these interfaces to compile.
+    React Native applications use React, but there is no DOM available. The JavaScript runtime
+    is ES6/ES2015 only. These definitions allow such projects to compile with only `--lib ES6`.
+
+    Warning: all of these interfaces are empty. If you want type definitions for various properties
+    (such as HTMLInputElement.prototype.value), you need to add `--lib DOM` (via command line or tsconfig.json).
+    */
+    // if (e.isComposing)
+    if (e.key !== "Enter" || !newTodo) return;
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  };
+
+  const todoItem = (todoText: string) => (
+    <li>
+      <div className="view">
+        <input className="toggle" type="checkbox" />
+        <label className="label">{todoText}</label>
+        <button className="destroy"></button>
+      </div>
+      <input className="edit" value={todoText} />
+    </li>
+  );
+
   return (
     <section className="todoapp">
       <div>
@@ -10,12 +39,18 @@ function App() {
           id="new-todo-title"
           className="new-todo"
           placeholder="할일을 추가해주세요"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          // onKeyDown으로 하면 한글 입력 시 isComposing 이슈로 인해 마지막 글자가 중복으로 입력되는데
+          // React.KeyboardEvent<T> interface에 isComposing 속성이 구현되어있지 않아서 onKeyUp으로 대체
+          onKeyUp={onPressEnter}
         />
       </div>
       <div className="main">
         <input className="toggle-all" type="checkbox" />
         <ul id="todo-list" className="todo-list">
-          <li>
+          {todos.map((todo) => todoItem(todo))}
+          {/* <li>
             <div className="view">
               <input className="toggle" type="checkbox" />
               <label className="label">새로운 타이틀</label>
@@ -38,7 +73,7 @@ function App() {
               <button className="destroy"></button>
             </div>
             <input className="edit" value="완료된 타이틀" />
-          </li>
+          </li> */}
         </ul>
       </div>
       <div className="count-container">
