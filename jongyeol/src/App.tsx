28b264ab2a92d103e4003d16caed5559c1c59d6b
jongyeol <pjr159@gmail.com>
2022-07-17 00:55:13 +0900

    feat: get todos from MSW

--- jongyeol/src/App.tsx
+++ jongyeol/src/App.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Todo from "./Component/Todo";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>todolist with typescript</h1>
-    </div>
-  );
+    const [todos, setTodos] = useState<any[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        fetchTodos();
+    }, []);
+
+    const fetchTodos = async () => {
+        try {
+            const res = await fetch("/todos");
+            const data = await res.json();
+            setTodos(data.todos);
+        } catch (err) {
+            console.error('err:', err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    if (loading) {
+        return <h2>loading...</h2>
+    }
+
+    return (
+        <>
+            <h1>TodoList</h1>
+             {todos?.map(todo => {
+                 return <Todo key={todo.title} title={todo.title} content={todo.content}/>
+             })}
+        </>
+    )
 }
 
 export default App;
---
+++ jongyeol/src/Component/Todo.tsx
@@ -0,0 +1,15 @@
+type TodoProps = {
+    title: String,
+    content: String
+}
+
+function Todo({title, content}: TodoProps) {
+    return(
+        <>
+            <h2>{title}</h2>
+            <div>{content}</div>
+        </>
+    )
+}
+
+export default Todo
