00765b0509258dc3a190154976c316a9ea6abc55
EonJo <sim04170@gmail.com>
2022-07-20 18:39:18 +0900

    [ add ] MSW 셋팅

--- hugo/src/App.tsx
+++ hugo/src/App.tsx
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from "react";
 import TodoApis from "@apis/todo";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {}, []);
+  // const [todos, setTodos] = useState([]);
+  // const [loading, setLoading] = useState(false);
+  // useEffect(() => {}, []);
 
   return <div className="App">This is Todo</div>;
 }
