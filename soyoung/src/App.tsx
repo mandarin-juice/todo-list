f04365ac6f462dfdcc66901dcb70d41ae742655f
sunivers <soyoungp02@gmail.com>
2022-07-24 23:48:49 +0900

    set todo-count, remove unused code

--- soyoung/src/App.tsx
+++ soyoung/src/App.tsx
@@ -17,25 +17,8 @@ function App() {
       </div>
       <div className="count-container">
         <span className="todo-count">
-          총 <strong>0</strong> 개
+          총 <strong>{todos?.length}</strong> 개
         </span>
-        <ul className="filters">
-          <li>
-            <a className="all selected" href="#/">
-              전체보기
-            </a>
-          </li>
-          <li>
-            <a className="active" href="#/active">
-              해야할 일
-            </a>
-          </li>
-          <li>
-            <a className="completed" href="#/completed">
-              완료한 일
-            </a>
-          </li>
-        </ul>
       </div>
     </section>
   );
