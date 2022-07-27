6578007b668cee13647ba488fc18ac97c3997979
sunivers <soyoungp02@gmail.com>
2022-07-27 18:55:22 +0900

    init server

---
+++ soyoung/back/index.js
@@ -0,0 +1,24 @@
+// CommonJs
+/**
+ * @type {import('fastify').FastifyInstance} Instance of Fastify
+ */
+const fastify = require("fastify")({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
