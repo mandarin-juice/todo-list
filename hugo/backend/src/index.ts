636c05bfecbb88a98577d139756bbde0835ae484
EonJo <sim04170@gmail.com>
2022-08-02 00:48:28 +0900

    [ modify ] 빌드셋팅 수정, 몽고DB 연결 추가

--- hugo/backend/src/index.ts
+++ hugo/backend/src/index.ts
@@ -1,15 +1,41 @@
-import fastify from 'fastify';
+import fastify from "fastify";
+import mongoose from "mongoose";
+import { TodoSchema } from "@schema/todo";
 
 const server = fastify();
 
-server.get('/ping', async (request, reply) => {
-  return 'pong\n';
+server.get("/ping", async (request, reply) => {
+  return "pong\n";
 });
 
-server.listen({ port: 9082 }, (err, address) => {
+server.get("/todos", async (request, reply) => {
+  const todos = mongoose.model("todos", TodoSchema).find();
+  console.log(todos);
+  return todos;
+});
+
+server.post("/todos", async (request, reply) => {
+  return "pong\n";
+});
+
+server.listen({ port: 9082 }, async (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  console.log(`Server listening at ${address}`);
+
+  const dbURL = process.env.DB_URL;
+  if (!dbURL) {
+    console.error("No database URL");
+    process.exit(1);
+  }
+
+  try {
+    await mongoose.connect(dbURL, {
+      dbName: "hugo",
+    });
+    console.log(`Server listening at ${address}`);
+  } catch (error: unknown) {
+    console.error(error);
+  }
 });
